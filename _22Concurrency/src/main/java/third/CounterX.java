2324cd23e42b482d8eb815b93c24391507fdb97b
grubneac <grubneac@yandex.ru>
2024-08-05 08:13:47 +0300

    add example of synchronized methos

---
+++ _22Concurrency/src/main/java/third/CounterX.java
@@ -0,0 +1,11 @@
+package third;
+
+class CounterX {
+    private int counter = 0;
+
+    public synchronized void increment() {                            // (1a)
+//public void increment() {                                         // (1b)
+        System.out.println(Thread.currentThread().getName()
+                + ": old:" + counter + " new:" + ++counter);
+    }
+}
