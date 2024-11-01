7ea3411e05a900363019eb02a8a5ea1e67579d69
grubneac <grubneac@yandex.ru>
2024-11-01 13:37:18 +0200

    Object Serialization

---
+++ _20JavaIO/src/main/java/serialization/CD.java
@@ -0,0 +1,9 @@
+package serialization;
+
+import java.io.Serializable;
+import java.time.Year;
+/** A record class that represents a CD. */
+public record CD(String artist, String title, int noOfTracks,
+                 Year year, Genre genre) implements Serializable {
+    public enum Genre implements Serializable {POP, JAZZ, OTHER}
+}
