5280af02d467e7fe4fbe10ec38cec48888c90c11
grubneac <grubneac@yandex.ru>
2024-11-01 16:31:51 +0200

    Customized Serialization

---
+++ _20JavaIO/src/main/java/serialization/Wheel.java
@@ -0,0 +1,13 @@
+package serialization;
+
+import java.io.Serializable;
+
+ public class Wheel implements Serializable {                   // (1a)
+//public class Wheel {                                              // (1b)
+    private int wheelSize;
+
+    public Wheel(int ws) { wheelSize = ws; }
+
+    @Override
+    public String toString() { return "wheel size: " + wheelSize; }
+}
