691199208351a06ddcc67cc54146018f80df92a9
grubneac <grubneac@yandex.ru>
2024-12-26 20:06:01 +0200

    just only connect to db

--- _24DbConnectivity/src/main/java/JDBCConnection.java
+++ _24DbConnectivity/src/main/java/JDBCConnection.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class JDBCConnection {
     public static void main(String[] args) {
-        final String jdbcURL = "jdbc:derby:musicDB;create=true;";
+        final String jdbcURL = "jdbc:derby:musicDB";
         try (Connection connection = DriverManager.getConnection(jdbcURL)) {
             /* use the connection. */
             System.out.println(connection);
