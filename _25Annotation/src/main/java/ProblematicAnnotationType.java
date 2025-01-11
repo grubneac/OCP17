57a929cb507c391a2be096743f886be2784545cc
grubneac <grubneac@yandex.ru>
2025-01-11 09:36:15 +0200

    Problematic Annotation Type

---
+++ _25Annotation/src/main/java/ProblematicAnnotationType.java
@@ -0,0 +1,6 @@
+public @interface ProblematicAnnotationType {
+//    StringBuilder message();              // Illegal return type.
+//    int[][] voting();                     // Only one-dimensional array allowed.
+//    String value;                         // Missing parentheses.
+//    private Thread.State state();         // Only public can be specified.
+}
