fb8a3187866f327dfc6d5988724a65b58f50ec08
grubneac <grubneac@yandex.ru>
2025-01-18 11:59:19 +0200

    Applying Normal Annotations

---
+++ _25Annotation/src/main/java/NuclearPlant.java
@@ -0,0 +1,6 @@
+@TaskInfo(                                                  // Normal annotation
+        taskDesc = "Class for monitoring nuclear reactor activity",       // Required
+        assignedTo = {"Tom", "Dick", "Harriet"},                          // Required
+        priority = TaskInfo.TaskPriority.HIGH                             // Optional
+)
+class NuclearPlant {}
